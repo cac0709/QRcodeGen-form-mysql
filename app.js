5f3fdb1d54983b565df766d32f9bcf1fde799af4
cac0709 <cac91053@gmail.com>
2020-06-01 16:14:20 +0800

    翡翠銀私

--- app.js
+++ app.js
@@ -60,9 +60,16 @@ var conn = mysql.createConnection({
             password : '123456',
             database : 'nodejs_login'
             });
-        var sql = 'select * from reservation'//取qrcode網址
-        var sql2 = 'select * from reservation'//取會議名稱
+        var sql = 'select * from reservation '//取qrcode網址
+        var sql2 = 'select * from reservation where starttime = CURRENT_TIME()'//取會議名稱
+        var sqlmonday = "SELECT * FROM nodejs_login.reservation where opendate = subdate(curdate(),date_format(curdate(),'%w')-1) order by starttime asc;"
+        var sqltwosday = "SELECT * FROM nodejs_login.reservation where opendate = subdate(curdate(),date_format(curdate(),'%w')-2) order by starttime asc ;"
+        var sqlthird = "SELECT * FROM nodejs_login.reservation where opendate = subdate(curdate(),date_format(curdate(),'%w')-3) order by starttime asc;"
+        var sqlforthday = "SELECT * FROM nodejs_login.reservation where opendate = subdate(curdate(),date_format(curdate(),'%w')-4) order by starttime asc;"
+        var sqlfriday = "SELECT * FROM nodejs_login.reservation where opendate = subdate(curdate(),date_format(curdate(),'%w')-5) order by starttime asc;"
+        var sqlfortest = 'Select * from reservation where dayofweek(opendate)=4;'
        
+
         conn.query(sql2,function(err,result2){
             console.log(result2)
             meetingname = result2;
@@ -70,8 +77,37 @@ var conn = mysql.createConnection({
         conn.query(sql,function(err,result){
             console.log(result);
             //給資料庫撈出來的資料定義
-            test = result[0].meetingroomcode;
-       
+            test = result;
+
+            conn.query(sqlmonday,function(err,resultmon){
+                console.log("星期一"+resultmon)
+                meetingnamemonday = resultmon;
+        
+                conn.query(sqltwosday,function(err,resulttwo){
+                    console.log("星期二"+resulttwo)
+                    meetingnametwo = resulttwo;
+
+                    conn.query(sqlthird,function(err,resultthird){
+                        console.log("星期三"+resultthird)
+                        meetingnamethird = resultthird;
+
+                        conn.query(sqlforthday,function(err,resultfour){
+                            console.log("星期四"+resultfour)
+                            meetingnamefour = resultfour;
+
+                            conn.query(sqlfriday,function(err,resultfri){
+                                console.log("星期五"+resultfri)
+                                meetingnamefri = resultfri;
+                
+            
+        
+    
+        
+        
+        //demo
+        conn.query(sqlfortest,function(err,resulttest){
+                    console.log(resulttest);
+                    realtest = resulttest;
 
         const fs = require('fs');
         const qrcode = require('qrcode');
@@ -93,7 +129,8 @@ var conn = mysql.createConnection({
           
                   <!-- Header -->
                       <header id="header">
-                          <div class="logo"><a href="#"><%= meetingname[0].meetingname %> <span>現在進行的會議</span></a></div>
+                       
+                          <div class="logo"><a href="#"><%= meetingname.meetingname %></a></div>
                           <h2>現在時間</h2>
                           <div id ="time"></div>
                           <section id="main">
@@ -111,8 +148,90 @@ var conn = mysql.createConnection({
           
                       
                       </header>
-                      
-                  
+                      <div id="sc">
+                      <table BORDER="1" align=center style="width:1500px;height:1000px;color:black;font-size:40px;font-family:DFKai-sb;">
+
+                      <tr>
+                          <td>時間/星期</td>
+                          <td>星期一</td>
+                          <td>星期二</td>
+                          <td>星期三</td>
+                          <td>星期四</td>
+                          <td>星期五</td>
+                      </tr>
+                      <tr>
+                          <td>7:00-8:00</td>
+                          <td><%=meetingnamemonday[0].meetingname %></td>
+                          <td><%=meetingnametwo[0] %>  </td>
+                          <td><%=meetingnamethird[0].meetingname %>  </td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                      <tr>
+                          <td>8:00-9:00</td>
+                          <td><%=meetingnamemonday[1] %></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                      <tr>
+                          <td>9:00-10:00</td>
+                          <td><%= meetingnamemonday[2]%></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                      <tr>
+                          <td>10:00-11:00</td>
+                          <td><%= meetingnamemonday[3]%> </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                      <tr>
+                          <td>11:00-12:00</td>
+                          <td><%= meetingnamemonday[4]%></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                      <tr>
+                          <td>13:00-14:00</td>
+                          <td><%= meetingnamemonday[5]%></td>
+                          <td></td>
+                          <td><%= realtest[0].meetingname%></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                          <td>14:00-15:00</td>
+                          <td><%= meetingnamemonday[6]%></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                      <tr>
+                      <td>15:00-16:00</td>
+                      <td><%= meetingnamemonday[7]%></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                     <tr>
+                          <td>17:00-18:00</td>
+                          <td><%= meetingnamemonday[8]%></td>
+                          <td>  </td>
+                          <td>  </td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                      </table>
+                    </div>
                       
                
           
@@ -139,9 +258,15 @@ var conn = mysql.createConnection({
           
           
         }
-        res.render('test3',{meetingname:meetingname});
+        res.render('test3',{meetingname:meetingname,meetingnamemonday:meetingnamemonday,meetingnametwo:meetingnametwo,meetingnamethird:meetingnamethird,meetingnamefour:meetingnamefour,meetingnamefri:meetingnamefri,realtest:realtest});
     })  
 })       
+})
+})
+})
+})
+})
+})
 })
       
     
